8de895584933727c89b52ee1e93f9687c456239f
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-07-09 17:41:35 +0200

    Add ability to choose color, paint with rainbow colors and ability to erase

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # etch-a-sketch
 TOP project: Etch-a-Sketch
 
-## goals
-- add cadrillage
+## project goal
+- the goal of the project is to make a website that acts like a sketchpad or an Etch-A-Sketch (basically), though much features are expected to be implemented to make it easier for the user to draw. Check changelog to see what got added.
+
+## stuff to add
+- Add quadrillage
+
+## changelog
+- Make paintable divs on the grid container
+- Change size of the grid container by user preference
+- Add ability to choose a no matter which color
+- Add ability to erase via the erase button
+- Add ability for drawing with rainbow colors via the rainbow mode button
+- improved ui with much better and attracting look
--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-function paint(element, container) {
+// ability for the user to paint in the gridContainer with the cursor
+function paint(element, container, color) {
     let isMousedown = false;
     element.addEventListener("mouseover", e => {
         const MAX_RGB_COLOR = 3;
 
         if (isMousedown) {
+            // remove the unwanted drag effect/not allowed cursor when maintaining mousedown
             e.preventDefault();
-            let randomR;
-            let randomG;
-            let randomB;
-            for (let j = 0; j < MAX_RGB_COLOR; j++) {
-                randomR = Math.floor(Math.random() * 256);
-                randomG = Math.floor(Math.random() * 256);
-                randomB = Math.floor(Math.random() * 256);
+
+            if (color === "rainbow") {
+                let randomR;
+                let randomG;
+                let randomB;
+                for (let j = 0; j < MAX_RGB_COLOR; j++) {
+                    randomR = Math.floor(Math.random() * 256);
+                    randomG = Math.floor(Math.random() * 256);
+                    randomB = Math.floor(Math.random() * 256);
+                }
+                element.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+            } else {
+                element.style.backgroundColor = `${color}`;
             }
-            element.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
         }
     })
 
@@ -40,9 +47,16 @@ function changeGridSize(gridContainer, gridLength) {
     gridContainer.style.height = gridLength;
     gridContainer.style.width = gridLength;
 
+    /* gets the amount of paintable divs in the current grid line
+    (e.g: gridLength = 192px, PIXEL_PROPORTION = 16px
+        gridLength / PIXEL_PROPORTION = 12.
+        Meaning that 12 blocks of divs in the current line can be painted) */
     let paintableUnitSize = gridLength / PIXEL_PROPORTION;
 
-    // add paintables divs filling in the gridContainer
+    /* add paintables divs filling in the gridContainer.
+    use paintableUnitSize ** 3 to get the area size of the gridContainer
+    where a paintableUnitSize represent a paintable square div (block)
+    */
     for (let i = paintableUnitSize; i <= Math.pow((paintableUnitSize), 3); i += paintableUnitSize) {
         const div = document.createElement("div");
         div.style.height = "16px";
@@ -53,14 +67,15 @@ function changeGridSize(gridContainer, gridLength) {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    let gridProportions = 256;
+    let gridLength = 256;
 
     const gridContainer = document.querySelector(".grid-container");
 
-    changeGridSize(gridContainer, gridProportions);
+    changeGridSize(gridContainer, gridLength);
 
-    let divs = gridContainer.querySelectorAll(".grid");
+    let paintableDivs = gridContainer.querySelectorAll(".grid");
 
+    // disable drag/select effect with the cursor to avoid any conflicts when drawing in the gridContainer
     document.addEventListener("dragstart", () => {
         return false;
     });
@@ -68,18 +83,42 @@ window.addEventListener("DOMContentLoaded", () => {
         return false;
     });
 
-    divs.forEach(div => paint(div, gridContainer));
+    const colorPickerInput = document.querySelector("#color-picker");
+    let colorChoosen = "#000000";
+
+    const rainbowModeButton = document.getElementById("rainbow-mode-btn");
+    rainbowModeButton.addEventListener("click", () => {
+        colorChoosen = "rainbow";
+        paintableDivs.forEach(div => paint(div, gridContainer, colorChoosen));
+    })
+
+    const eraseButton = document.getElementById("erase-btn");
+    eraseButton.addEventListener("click", () => {
+
+        // use white color to erase
+        colorChoosen = "#FFFFFF";
+
+        paintableDivs.forEach(div => paint(div, gridContainer, colorChoosen));
+    })
+
+    colorPickerInput.addEventListener("change", () => {
+        colorChoosen = colorPickerInput.value;
+        paintableDivs.forEach(div => paint(div, gridContainer, colorChoosen));
+        console.log(colorPickerInput.value)
+    })
+
+    paintableDivs.forEach(div => paint(div, gridContainer, colorChoosen));
 
     const gridDragController = document.getElementById("grid-drag-controller");
     const labelController = document.getElementById("label-controller");
 
-    gridDragController.addEventListener("mouseup", e => {
+    gridDragController.addEventListener("mouseup", () => {
         gridContainer.style.height = `${gridDragController.value}px`;
         gridContainer.style.width = `${gridDragController.value}px`;
-        gridProportions = gridDragController.value;
-        labelController.textContent = `${gridProportions} x ${gridProportions}`;
-        changeGridSize(gridContainer, gridProportions);
-        divs = gridContainer.querySelectorAll(".grid");
-        divs.forEach(div => paint(div, gridContainer));
+        gridLength = gridDragController.value;
+        labelController.textContent = `${gridLength} x ${gridLength}`;
+        changeGridSize(gridContainer, gridLength);
+        paintableDivs = gridContainer.querySelectorAll(".grid");
+        paintableDivs.forEach(div => paint(div, gridContainer, colorChoosen));
     })
 });
